60a4447d0d0b6869b3834f7546bdd47aba6fbda1
hsoulsboy <hsoulsboy@gmail.com>
2023-01-14 16:44:48 -0300

    Improve pagination and preference system

--- steamSearcher/game_db.py
+++ steamSearcher/game_db.py
@@ -1,14 +1,54 @@
 import requests
 import re
+import os
+import sys
 
-def main():
-    base_url = "https://store.steampowered.com/search?ignore_preferences=1&ndl=1&page={}"
+def create(output_file, filter_preferences=False, filter_language=False):
+    """Create a CSV file containing a list of games Ids and Names
+
+    Parameters
+    ----------
+    output_file : String
+        CSV file output path
+
+    filter_preferences : bool, optional (default is False)
+        Flag to filter Steam results based on the user's preferences
+
+    filter_language : bool, optional (default is False)
+        Flag to filter Steam results based on the user's language
+
+    Returns
+    -------
+    Dict
+        Dictionary containing information of all games based on the pagination
+    """
+
+    if os.path.isfile(output_file) or os.path.isdir(output_file):
+        print("Output must be a non used path.")
+        sys.exit(0)
+
+    preferences, language = (1, 1)
+    if filter_preferences is True:
+        preferences = 0
+    elif filter_language is True:
+        language = 0
+
+    base_url = "https://store.steampowered.com/search?ignore_preferences={}&ndl={}&page={}"
+
+    page = 1
+    page_count = 0
+    resp = requests.get(base_url.format(preferences, language, page))
+    for line in resp.text.split(">"):
+        if "SearchLinkClick" in line:
+            resp_game = re.search(f'page=([0-9]+)', line)
+            if int(resp_game.group(1)) > 3:
+                        page_count = resp_game.group(1)
+
+    print(page_count)
 
     steam_links = {}
-    page = 1
-
-    while page <= 5464:
-        resp = requests.get(base_url.format(page))
+    while page <= int(page_count):
+        resp = requests.get(base_url.format(preferences, language, page))
         steam_links[page] = []
         for line in resp.text.split(">"):
             if "https://store.steampowered.com/app/" in line:
@@ -19,11 +59,8 @@ def main():
                 game_links.append(game_link)
                 steam_links[page] = game_links
 
-                with open('games.csv', 'a') as file:
+                with open(output_file, 'a') as file:
                     file.write(f'{game_link[0]},{game_link[1]}\n')
         page += 1
 
-    print(steam_links)
-
-if __name__ == "__main__":
-    main()
+    return steam_links
